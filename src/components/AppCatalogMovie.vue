e71be020f6a066066b89e3e57ec4259ad6d4a39e
Paracells <enoalife@hotmail.com>
2021-05-31 14:47:42 +0300

    broken catalog

--- src/components/AppCatalogMovie.vue
+++ src/components/AppCatalogMovie.vue
@@ -1,26 +1,29 @@
- <template>
+<template>
   <div class="relative" @mouseover="show = true" @mouseout="show = false">
-    <div class="bg-gray-800 shadow-md rounded-3xl p-2 cursor-pointer">
-      <img
-        :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
-        :alt="movie.original_title"
-        class="h-auto rounded-2xl object-fit w-full transform transition duration-500"
-        :class="imgHover"
-      />
-    </div>
+    <router-link :to="{name:'TheMovie', params:{id:movie.id}}">
+      <div class="bg-gray-800 shadow-md rounded-3xl p-2 cursor-pointer">
+        <img
+            :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
+            :alt="movie.original_title"
+            class="h-auto rounded-2xl object-fit w-full transform transition duration-500"
+            :class="imgHover"
+        />
+      </div>
+    </router-link>
     <div
-      class="text-white bg-indigo-400 bg-opacity-50 text-opacity-80 w-5/6 text-center absolute top-5 left-7 text-xl font-semibold rounded-lg transform transition duration-500"
-      :class="titleHover"
+        class="text-white bg-indigo-400 bg-opacity-50 text-opacity-80 w-5/6 text-center absolute top-5 left-7 text-xl font-semibold rounded-lg transform transition duration-500"
+        :class="titleHover"
     >
       {{ movie.original_title }}
     </div>
     <div
-      class="text-green-100 w-5/6 text-center absolute bottom-5 left-7 transform transition duration-1000"
-      :class="linkHover"
+        class="text-green-100 w-5/6 text-center absolute bottom-5 left-7 transform transition duration-1000"
+        :class="linkHover"
     >
       {{ overView }}
     </div>
   </div>
+
 </template>
 
 <script>
@@ -54,8 +57,8 @@ export default {
   computed: {
     overView() {
       return this.movie.overview.split(" ").length > 50
-        ? this.movie.overview.split(" ").splice(0, 50).join(" ").concat("...")
-        : this.movie.overview;
+          ? this.movie.overview.split(" ").splice(0, 50).join(" ").concat("...")
+          : this.movie.overview;
     },
   },
   watch: {
